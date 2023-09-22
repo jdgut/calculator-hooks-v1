6f331904c91a93342523b38575fb8716397075d2
Shane Bell <being.for.itself@gmail.com>
2023-09-22 19:19:23 -0400

    The calculator works!

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,115 @@
-import React, {useState, useEffect} from "react";
-import Button from './components/Button';
-
+import React, { useState, useRef } from "react";
+import Button from "./components/Button";
 
 const App = () => {
-    const [resultValue, setResultValue] = useState('0');
-    const [aggregateValue, setAggregateValue] = useState(0);
+  const [dispValue, setDispValue] = useState("0");
+  const resultValue = useRef(0);
+  const aggregateValue = useRef(0);
+  const currentOp = useRef(null);
 
-    const buttonHandler = (input) => {
-        switch(input) {
-            case 'AC':
-                setResultValue('0');
-                setAggregateValue(0);
-            break;
-            case '+/-':
-                const currentValue = resultValue;
-                const displayValue = currentValue.indexOf('-') >= 0 ? currentValue.replace('-','') : '-' + currentValue;
-                setResultValue(displayValue);
-            break;
-            case '+':
-            break;
-            case '':
-            break;
-            case '':
-            break;
-            case '':
-            break;
-            case '':
-            break;
-            case '':
-            break;
-            default:
-                const displayResult = resultValue === 0 ? input : resultValue + '' + input;
-                setResultValue(displayResult);
-            break;
-        }
+  const calculate = () => {
+    if (currentOp.current === "+") {
+      resultValue.current = aggregateValue.current + resultValue.current;
+    } else if (currentOp.current === "-") {
+      resultValue.current = aggregateValue.current - resultValue.current;
+    } else if (currentOp.current === "/") {
+      resultValue.current = aggregateValue.current / resultValue.current;
+    } else if (currentOp.current === "X") {
+      resultValue.current = aggregateValue.current * resultValue.current;
+    } else if (currentOp.current === "%") {
+      resultValue.current = aggregateValue.current % resultValue.current;
     }
+    aggregateValue.current = 0;
+    currentOp.current = null;
+    setDispValue(resultValue.current.toString());
+  };
 
-    const addNumber = (a, b) => {
-        const aNum = Number(a);
-        const bNum = Number(b);
-        return aNum + bNum;
+  const buttonHandler = (input) => {
+    switch (input) {
+      case "AC":
+        resultValue.current = 0;
+        aggregateValue.current = 0;
+        currentOp.current = null;
+        setDispValue(resultValue.current.toString());
+        break;
+      case "+/-":
+        resultValue.current = resultValue.current * -1;
+        setDispValue(resultValue.current.toString());
+        break;
+      case "+":
+        setOp("+");
+        break;
+      case "-":
+        setOp("-");
+        break;
+      case "X":
+        setOp("X");
+        break;
+      case "/":
+        setOp("/");
+        break;
+      case "%":
+        setOp("%");
+        break;
+      case ".":
+        setDispValue(dispValue + ".");
+        break;
+      case "=":
+        calculate();
+        break;
+      default:
+        const displayResult = dispValue === "0" ? input : dispValue + input;
+        resultValue.current = parseFloat(displayResult);
+        setDispValue(resultValue.current.toString());
+        break;
     }
+  };
 
-    const calculatorButtons = [
-        ['AC', '+/-', '%', '/'],
-        ['7', '8', '9', 'X'],
-        ['4', '5', '6', '-'],
-        ['1', '2', '3', '+'],
-        ['0', '.', '='],
-    ];
+  const setOp = (op) => {
+    aggregateValue.current = resultValue.current;
+    resultValue.current = 0;
+    currentOp.current = op;
+    setDispValue("0");
+  };
 
-    const gridResultSet = calculatorButtons.map( (rowData, rowIdx, rowArr) => {
-        return (
-            <div className="row" key={rowIdx}>
-            {
-                rowData.map( (colData, colIdx) => {
-                    const className = (rowIdx === (rowArr.length - 1)) && colIdx === 0 ? 'col-6' : 'col-3'; 
-                    return (
-                        <div className={className} key={colIdx}>
-                            <Button buttonValue={colData} buttonHandler={buttonHandler} />
-                        </div>
-                    )
-                })
-            }
-            </div>
-        )
-    });
+  // const addNumber = (a, b) => {
+  //   const aNum = Number(a);
+  //   const bNum = Number(b);
+  //   return aNum + bNum;
+  // };
 
+  const calculatorButtons = [
+    ["AC", "+/-", "%", "/"],
+    ["7", "8", "9", "X"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="],
+  ];
+
+  const gridResultSet = calculatorButtons.map((rowData, rowIdx, rowArr) => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col">{resultValue}</div>
+      <div className="row" key={rowIdx}>
+        {rowData.map((colData, colIdx) => {
+          const className =
+            rowIdx === rowArr.length - 1 && colIdx === 0 ? "col-6" : "col-3";
+          return (
+            <div className={className} key={colIdx}>
+              <Button buttonValue={colData} buttonHandler={buttonHandler} />
             </div>
-            {gridResultSet}
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">{dispValue}</div>
+      </div>
+      {gridResultSet}
+    </div>
+  );
+};
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Button = (props) => {
-    const digit = props.buttonValue;
-    const buttonHandler = props.buttonHandler;
-    return (
-        <>
-            <button onClick={() => buttonHandler(digit)}>{digit}</button>
-        </>
-    )
-}
+  const digit = props.buttonValue;
+  const buttonHandler = props.buttonHandler;
+  return (
+    <>
+      <button onClick={() => buttonHandler(digit)}>{digit}</button>
+    </>
+  );
+};
 
 export default Button;
